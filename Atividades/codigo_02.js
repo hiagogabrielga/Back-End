10b1a0466ba90be363373cd2f4f6c363275b50c8
HiagoGGA <hiagogabriel1132@gmail.com>
2024-03-21 09:40:04 -0400

    a

--- Atividades/codigo_02.js
+++ Atividades/codigo_02.js
@@ -4,6 +4,7 @@ var apartamento = {
     tipo: "apartamento",
     endereco: "Avenida Principal, 456 - Centro",
     andar: 7,
+    
 };
 console.log(`${apartamento.tipo} com ${apartamento.quarto} quartos, localizado no ${apartamento.andar}º andar da ${apartamento.endereco}.`);
 
