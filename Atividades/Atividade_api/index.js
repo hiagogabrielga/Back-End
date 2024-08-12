c95ee92e0b755556da1c7ba2247fd4cc51127699
hiagogabrielga <hiagogabriel1132@gmail.com>
2024-08-12 11:44:09 -0400

    a

--- Atividades/Atividade_api/index.js
+++ Atividades/Atividade_api/index.js
@@ -3,13 +3,8 @@ const app = express();
 
 let data = new Date();
 
-let mensagemDoDia = require('./mensagemDoDia');
-
 app.get('/', (req, res) => {
-    let dia = data.getDate();
-    let mensagemSelecionada = mensagemDoDia.retornarMensagemDoDia(dia);
-
-    res.json({mensagem: mensagemSelecionada});
+    res.send('Hello word!');
 });
 
 app.listen(8080, () => {
